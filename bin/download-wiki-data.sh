8e7d4bbc03e1acea71f837b6a9571bd7bd7eec7d
Ben Wing <ben@benwing.com>
2014-06-17 01:26:09 -0500

    big/download-wiki-data.sh: Add -np to wget -r

--- bin/download-wiki-data.sh
+++ bin/download-wiki-data.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-wget -r http://web.corral.tacc.utexas.edu/utcompling/fieldspring-data/wistr-models-cwardev-gt
-wget -r http://web.corral.tacc.utexas.edu/utcompling/fieldspring-data/wistr-models-cwartest-gt
-wget -r http://web.corral.tacc.utexas.edu/utcompling/fieldspring-data/wistr-models-trdev-gt
-wget -r http://web.corral.tacc.utexas.edu/utcompling/fieldspring-data/wistr-models-trtest-gt
+wget -np -r http://web.corral.tacc.utexas.edu/utcompling/fieldspring-data/wistr-models-cwardev-gt
+wget -np -r http://web.corral.tacc.utexas.edu/utcompling/fieldspring-data/wistr-models-cwartest-gt
+wget -np -r http://web.corral.tacc.utexas.edu/utcompling/fieldspring-data/wistr-models-trdev-gt
+wget -np -r http://web.corral.tacc.utexas.edu/utcompling/fieldspring-data/wistr-models-trtest-gt
 wget http://web.corral.tacc.utexas.edu/utcompling/fieldspring-data/wiki/enwiki-cwardev-20spd-100.log
 wget http://web.corral.tacc.utexas.edu/utcompling/fieldspring-data/wiki/enwiki-cwartest-20spd-100.log
 wget http://web.corral.tacc.utexas.edu/utcompling/fieldspring-data/wiki/enwiki-trconlldev-100.log
