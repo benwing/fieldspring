14390504ef970e3901c8c8395d2275cbd393121d
Ben Wing <ben@benwing.com>
2014-06-23 20:38:00 -0500

    SplitDevTest: Add second arg to specify what fraction goes in test vs. dev

--- src/main/scala/opennlp/fieldspring/tr/app/SplitDevTest.scala
+++ src/main/scala/opennlp/fieldspring/tr/app/SplitDevTest.scala
@@ -4,6 +4,10 @@ import java.io._
 
 object SplitDevTest extends App {
   val dir = new File(args(0))
+  // What fraction goes in "test". 3 means 1/3 in test, 5 means 1/5, etc.
+  val testfrac =
+    if (args.size > 1) args(1).toInt
+    else 3
 
   val devDir = new File(dir.getCanonicalPath+"dev")
   val testDir = new File(dir.getCanonicalPath+"test")
@@ -14,7 +18,7 @@ object SplitDevTest extends App {
 
   var i = 1
   for(file <- files) {
-    if(i % 3 == 0)
+    if(i % testfrac == 0)
       file.renameTo(new File(testDir, file.getName))
     else
       file.renameTo(new File(devDir, file.getName))
