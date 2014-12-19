fe9ee03d54b079495f48ffa5d9684e71fcdf15e7
Ben Wing <ben@benwing.com>
2014-12-19 07:03:54 -0600

    ConvertCwarToGoldCorpus/prepare-cwar.sh: Major rewrite, make it more robust and include files in sorted order

--- bin/prepare-cwar.sh
+++ bin/prepare-cwar.sh
@@ -5,10 +5,25 @@ if [ -z $FIELDSPRING_DIR ]; then
     exit
 fi
 
-origcwarxmldir=${1%/}
-pathtokml=$2
-pathtogaz=$3
-cwarxmloutdir=${4%/}
+origcwarxmldir=
+pathtokml=
+pathtotgn=
+pathtogaz=
+cwarxmloutdir=
+keeptmp=no
+
+# Handle command-line args for us.
+while true ; do
+  case "$1" in
+    -c | --corpus ) origcwarxmldir="${2%/}"; shift 2 ;;
+    -k | --kml ) pathtokml="$2"; shift 2 ;;
+    -t | --tgn ) pathtotgn="$2"; shift 2 ;;
+    -g | --gaz ) pathtogaz="$2"; shift 2 ;;
+    -o | --outdir ) cwarxmloutdir="${2%/}"; shift 2 ;;
+    --keep-tmp ) keeptmp=yes; shift ;;
+    * ) break ;;
+  esac
+done
 
 echo "Converting original Cwar corpus to plain format..."
 cwarxml2txttgn.sh $origcwarxmldir cwarplaintgn
@@ -24,14 +39,35 @@ if [ ! -e $cwarxmloutdir/test ]; then
     mkdir $cwarxmloutdir/test
 fi
 
+pathtotgncvt=
+coordargs=
+if [ -n "$pathtotgn" ]; then
+  echo "Converting TGN coordinates to a simple text file..."
+  pathtotgncvt=tgn-coords.$$.txt
+  if echo "$pathtotgn" | grep '\.bz2$' > /dev/null ; then
+    bzcat "$pathtotgn" | convert-TGNOut_Coordinates.nt.sh > $pathtotgncvt
+  else
+    convert-TGNOut_Coordinates.nt.sh < "$pathtotgn" > $pathtotgncvt
+  fi
+  coordargs="$coordargs -t $pathtotgncvt"
+fi
+if [ -n "$pathtokml" ]; then
+  coordargs="$coordargs -k $pathtokml"
+fi
+
 echo "Converting dev corpus to Fieldspring format..."
-fieldspring --memory 8g run opennlp.fieldspring.tr.app.ConvertCwarToGoldCorpus cwarplaintgndev $pathtokml $pathtogaz > $cwarxmloutdir/dev/cwar-dev.xml
+fieldspring --memory 15g run opennlp.fieldspring.tr.app.ConvertCwarToGoldCorpus -c cwarplaintgndev $coordargs -g $pathtogaz > $cwarxmloutdir/dev/cwar-dev.xml
 echo "Converting test corpus to Fieldspring format..."
-fieldspring --memory 8g run opennlp.fieldspring.tr.app.ConvertCwarToGoldCorpus cwarplaintgntest $pathtokml $pathtogaz > $cwarxmloutdir/test/cwar-test.xml
+fieldspring --memory 15g run opennlp.fieldspring.tr.app.ConvertCwarToGoldCorpus -c cwarplaintgntest $coordargs -g $pathtogaz > $cwarxmloutdir/test/cwar-test.xml
 
-echo "Deleting temporary files..."
-rm -rf cwarplaintgn
-rm -rf cwarplaintgndev
-rm -rf cwarplaintgntest
+if [ "$keeptmp" != "yes" ]; then
+  echo "Deleting temporary files..."
+  rm -rf cwarplaintgn
+  rm -rf cwarplaintgndev
+  rm -rf cwarplaintgntest
+  if [ -n "$pathtotgncvt" ]; then
+    rm -rf $pathtotgncvt
+  fi
+fi
+
 echo "Done."
-
--- src/main/scala/opennlp/fieldspring/tr/app/ConvertCwarToGoldCorpus.scala
+++ src/main/scala/opennlp/fieldspring/tr/app/ConvertCwarToGoldCorpus.scala
@@ -6,51 +6,92 @@ import tr.topo._
 import tr.util._
 import tr.topo.gaz._
 import tr.text.io._
-import java.util.zip._
+import tr.text.prep._
 
+import org.clapper.argot._
+import ArgotConverters._
+
+import java.util.zip._
 import java.io._
 
 import scala.collection.JavaConversions._
+import scala.collection.mutable
 
 object ConvertCwarToGoldCorpus extends App {
 
+  val parser = new ArgotParser("fieldspring run opennlp.fieldspring.tr.app.ConvertCwarToGoldCorpus", preUsage = Some("Fieldspring"))
+
+  val corpusDirParam = parser.option[String](List("c", "corpus"), "corpus", "raw (XML) corpus dir")
+  val kmlInputFileParam = parser.option[String](List("k", "kml"), "kml", "gold KML input file with coordinates")
+  val tgnInputFileParam = parser.option[String](List("t", "tgn"), "tgn", "text file with direct TGN-to-coordinate mapping")
+  val gazInputFileParam = parser.option[String](List("g", "gaz"), "gaz", "serialized gazetteer input file")
+
   val digitsRE = """^\d+$""".r
   val floatRE = """^-?\d*\.\d*$""".r
+  val toponymWithSpacesRE = """tgn,(\d+)-(.*?)-]]""".r
   val toponymRE = """^tgn,(\d+)-(.+)-]][^\s]*$""".r
 
-  val corpusFiles = new File(args(0)).listFiles
-  val goldKml = scala.xml.XML.loadFile(args(1))
-  val gazIn = args(2)
+  val tokenizer = new OpenNLPTokenizer
 
+  def errprint(str: String) {
+    System.err.println(str)
+  }
 
-  val tgnToCoord =
-  //(goldKml \\ "Placemark").foreach { placemark =>
-  (for(placemark <- (goldKml \\ "Placemark")) yield {
-    var tgn = -1
-    (placemark \\ "Data").foreach { data =>
-      val name = (data \ "@name").text
-      if(name.equals("tgn")) {
-        val text = data.text.trim
-        if(digitsRE.findFirstIn(text) != None)
-          tgn = text.toInt
-      }
+  try {
+    parser.parse(args)
+  }
+  catch {
+    case e: ArgotUsageException => errprint(e.message); sys.exit(1)
+  }
+
+  if (tgnInputFileParam == None && gazInputFileParam == None) {
+    errprint("Either -t (--tgn) or -k (--kml), or both, need to be specified"); sys.exit(1)
+  }
+  val corpusFiles =
+    new File(corpusDirParam.value.get).listFiles.sortBy(_.getName)
+  val maybeGoldKml = kmlInputFileParam.value.map(scala.xml.XML.loadFile)
+  val gazIn = gazInputFileParam.value.get
+
+  val tgnToCoordKml =
+    maybeGoldKml.map { goldKml =>
+      //(goldKml \\ "Placemark").foreach { placemark =>
+      (for(placemark <- (goldKml \\ "Placemark")) yield {
+        var tgn = -1
+        (placemark \\ "Data").foreach { data =>
+          val name = (data \ "@name").text
+          if(name.equals("tgn")) {
+            val text = data.text.trim
+            if(digitsRE.findFirstIn(text) != None)
+              tgn = text.toInt
+          }
+        }
+        var coordsRaw = ""
+        (placemark \\ "coordinates").foreach { coordinates =>
+          coordsRaw = coordinates.text.trim.dropRight(2)
+        }
+
+        val coordsSplit = coordsRaw.split(",")
+
+        if(tgn == -1 || coordsSplit.size != 2 || floatRE.findFirstIn(coordsSplit(0)) == None
+           || floatRE.findFirstIn(coordsSplit(1)) == None)
+          None
+        else {
+          val lng = coordsSplit(0).toDouble
+          val lat = coordsSplit(1).toDouble
+          Some((tgn, Coordinate.fromDegrees(lat, lng)))
+        }
+      }).flatten.toMap
     }
-    var coordsRaw = ""
-    (placemark \\ "coordinates").foreach { coordinates =>
-      coordsRaw = coordinates.text.trim.dropRight(2)
+  val tgnToCoordTgn =
+    tgnInputFileParam.value.map { tgnInputFile =>
+      (for {line <- localfh.openr(tgnInputFile)
+            Array(tgncode, lat, long) = line.split(" ")
+            tgn = tgncode.toInt
+            coord = Coordinate.fromDegrees(lat.toDouble, long.toDouble)
+           } yield (tgn -> coord)
+      ).toMap
     }
-
-    val coordsSplit = coordsRaw.split(",")
-
-    if(tgn == -1 || coordsSplit.size != 2 || floatRE.findFirstIn(coordsSplit(0)) == None
-       || floatRE.findFirstIn(coordsSplit(1)) == None)
-      None
-    else {
-      val lng = coordsSplit(0).toDouble
-      val lat = coordsSplit(1).toDouble
-      Some((tgn, Coordinate.fromDegrees(lat, lng)))
-    }
-  }).flatten.toMap
+  val tgnToCoord = (tgnToCoordKml ++ tgnToCoordTgn).reduce(_ ++ _)
   
   //tgnToCoord.foreach(println)
 
@@ -71,60 +112,113 @@ object ConvertCwarToGoldCorpus extends App {
   println("<?xml version=\"1.0\" encoding=\"UTF-8\"?>")
   println("<corpus>")
 
+  def escapeXML(form: String) = {
+    // FIXME: This is kind of bogus. Instances of & will generally already
+    // be entities, so we should preserve them. However, tokenization with
+    // OpenNLP will generally separate off the semicolon, and stripPunc
+    // will strip trailing semicolons and leading ampersands, so we'd need
+    // some work to keep the entities intact.
+    form.replaceAll("&", "&amp;").replaceAll("\"", "&quot;")
+  }
+
+  def processToken(token: String) {
+    val strippedToken = escapeXML(TextUtil.stripPunc(token))
+    if (strippedToken.length > 0
+      // && CorpusXMLWriter.isSanitary(strippedToken)
+    )
+      println("      <w tok=\""+strippedToken+"\"/>")
+  }
+
+  def processText(form: String) {
+    for(tok <- tokenizer.tokenize(form))
+      processToken(tok)
+  }
+
+  def processToponym(form: String, tgnCode: Int) {
+    val candidates = gaz.lookup(form.toLowerCase)
+    val goldCoord = tgnToCoord.getOrElse(tgnCode, null)
+    if(candidates == null || goldCoord == null) {
+      // If doesn't match and toponym contains a comma, try to make a
+      // toponym out of just the part before the comma. (FIXME: Perhaps
+      // we should also try to make a toponym out of the rest, which will
+      // normally be a state or country. Similar to ToponymWikiSource,
+      // we could match on a toponym of type STATE whose location is within
+      // 500 km or so of the gold location.)
+      if (form contains ',') {
+        val splitform = form.split(", *", 2)
+        processToponym(splitform(0), tgnCode)
+        processToken(",")
+        processText(splitform(1))
+      } else {
+        processText(form)
+      }
+    }
+    else {
+      var matchingCand:Location = null
+      var minDist = Double.MaxValue
+      // Find minimum distance, ensure it's at most 50km
+      for(cand <- candidates) {
+        val dist = cand.getRegion.getCenter.distanceInKm(goldCoord)
+        if (dist < minDist) {
+          minDist = dist
+          matchingCand = cand
+        }
+      }
+      if (matchingCand == null) {
+        processText(form)
+      } else if (minDist > (
+          if (matchingCand.getType == Location.Type.STATE) 500 else 50
+        )) {
+        //errprint(s"Rejecting closest candidate %s,%s for %s because distance %.2f km is too much" format (
+        //  matchingCand.getName, matchingCand.getAdmin1Code, form, minDist))
+        processText(form)
+      }
+      //val formToWrite = if(CorpusXMLWriter.isSanitary(form)) form else "MALFORMED"
+      else { //if(CorpusXMLWriter.isSanitary(form))
+        println("      <toponym term=\""+escapeXML(form)+"\">")
+        println("        <candidates>")
+        for(cand <- candidates) {
+          val region = cand.getRegion
+          val center = region.getCenter
+          print("          <cand id=\""+cand.getId+"\" lat=\""+center.getLatDegrees+"\" long=\""
+                +center.getLngDegrees+"\" type=\""+cand.getType+"\" admin1code=\""
+                +cand.getAdmin1Code+"\"")
+          if(matchingCand == cand)
+            print(" selected=\"yes\"")
+          println("/>")
+          /*println("            <representatives>")
+          for(rep <- region.getRepresentatives) {
+            println("              <rep lat=\""+rep.getLatDegrees+"\" long=\""+rep.getLngDegrees+"\"/>")
+          }
+          println("            </representatives>")*/
+        }
+        println("        </candidates>")
+        println("      </toponym>")
+      }
+    }
+  }
+
   for(file <- corpusFiles) {
     println("  <doc id=\""+file.getName+"\">")
 
     for(line <- localfh.openr(file.toString).map(_.trim).filter(_.length > 0)) {
+      val toponyms = mutable.Map[String, (String, Int)]()
+      var nextToponym = 0
       println("    <s>")
-      for(token <- line.split(" ")) {
-        if(toponymRE.findFirstIn(token) != None) {
-          val toponymRE(tgnRaw, formRaw) = token
-          val form = formRaw.replaceAll("-", " ")
-          val candidates = gaz.lookup(form.toLowerCase)
-          val goldCoord = tgnToCoord.getOrElse(tgnRaw.toInt, null)
-          if(candidates == null || goldCoord == null) {
-            for(tok <- form.split(" ").filter(t => CorpusXMLWriter.isSanitary(t)))
-              println("      <w tok=\""+tok+"\"/>")
-          }
-          else {
-            var matchingCand:Location = null
-            for(cand <- candidates) {
-              if(cand.getRegion.getCenter.distanceInMi(goldCoord) < 5.0) {
-                matchingCand = cand
-              }
-            }
-            if(matchingCand == null) {
-              for(tok <- form.split(" ").filter(t => CorpusXMLWriter.isSanitary(t)))
-                println("      <w tok=\""+tok+"\"/>")
-            }
-            //val formToWrite = if(CorpusXMLWriter.isSanitary(form)) form else "MALFORMED"
-            else if(CorpusXMLWriter.isSanitary(form)) {
-              println("      <toponym term=\""+form+"\">")
-              println("        <candidates>")
-              for(cand <- candidates) {
-                val region = cand.getRegion
-                val center = region.getCenter
-                print("          <cand id=\""+cand.getId+"\" lat=\""+center.getLatDegrees+"\" long=\""
-                      +center.getLngDegrees+"\" type=\""+cand.getType+"\" admin1code=\""
-                      +cand.getAdmin1Code+"\"")
-                if(matchingCand == cand)
-                  print(" selected=\"yes\"")
-                println("/>")
-                /*println("            <representatives>")
-                for(rep <- region.getRepresentatives) {
-                  println("              <rep lat=\""+rep.getLatDegrees+"\" long=\""+rep.getLngDegrees+"\"/>")
-                }
-                println("            </representatives>")*/
-              }
-              println("        </candidates>")
-              println("      </toponym>")
-            }
-          }
-        }
-        else {
-          val strippedToken = TextUtil.stripPunc(token)
-          if(CorpusXMLWriter.isSanitary(/*BaseApp.CORPUS_FORMAT.PLAIN, */strippedToken))
-            println("      <w tok=\""+strippedToken+"\"/>")
+      val fixedline = toponymWithSpacesRE.replaceAllIn(line, m => {
+        val form = m.group(2).replaceAll("-", " ")
+        val tgnCode = m.group(1).toInt
+        val index = s"qtoponymq$nextToponym"
+        nextToponym += 1
+        toponyms(index) = (form, tgnCode)
+        s" $index "
+      })
+      for (token <- tokenizer.tokenize(fixedline.replaceAll("-]]", ""))) {
+        if (toponyms contains token) {
+          val (form, tgnCode) = toponyms(token)
+          processToponym(form, tgnCode)
+        } else {
+          processToken(token)
         }
       }
       println("    </s>")
